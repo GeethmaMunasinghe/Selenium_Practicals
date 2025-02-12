be87a4a162fd74f5d3e5fa5ebe07ce3c5b1ece3a
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-02-12 19:50:20 +0530

    Interacting with elements

--- selenium_learning/src/test/java/OpenGoogleTest.java
+++ selenium_learning/src/test/java/OpenGoogleTest.java
@@ -1,4 +1,6 @@
 import com.google.common.annotations.VisibleForTesting;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -7,8 +9,11 @@ public class OpenGoogleTest {
 
     @Test
     public void googleTest(){
+        System.setProperty("webdriver.chrome.driver","D:\\Level 4\\L4 S1\\Selenium Practice\\Driver\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver=new ChromeDriver(); //Trigger the test case
         driver.get("https://www.google.com/");
-        driver.quit();
+
+        driver.findElement(By.name("q")).sendKeys("Colombo"+ Keys.ENTER);
+
     }
 }
