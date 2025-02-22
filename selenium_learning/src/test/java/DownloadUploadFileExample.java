065422435884c992193e30c2822756de3cd9302f
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-02-22 21:35:00 +0530

    completed uploadFile first method

--- selenium_learning/src/test/java/DownloadUploadFileExample.java
+++ selenium_learning/src/test/java/DownloadUploadFileExample.java
@@ -1,10 +1,14 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 import java.io.File;
 
 public class DownloadUploadFileExample {
@@ -38,8 +42,36 @@ public class DownloadUploadFileExample {
     }
 
     @Test
-    public void uplaodFileTest(){
+    public void uplaodFileTest() throws AWTException, InterruptedException {
         driver.get("https://www.leafground.com/file.xhtml");
+
+        //first way: using Robot class
+        WebElement uploadBtn=driver.findElement(By.id("j_idt88:j_idt89"));
+        uploadBtn.click();
+
+        //windows control begin here
+        String data="E:\\chrome\\hd-black-wallpaper-whatspaper.jpg";
+        StringSelection selection=new StringSelection(data);
+
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection,null);
+
+        Thread.sleep(4000);
+
+        Robot robot=new Robot();
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+
+        Thread.sleep(4000);
+
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+
+
+
+        //second way: using sendKeys
+
 
     }
 }
