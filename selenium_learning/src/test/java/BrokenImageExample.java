cdd360430a511d8d0ef08546a4bf009b4fc46c7b
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-02-25 20:05:45 +0530

    create finBrokenImage method

--- selenium_learning/src/test/java/BrokenImageExample.java
+++ selenium_learning/src/test/java/BrokenImageExample.java
@@ -1,7 +1,11 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class BrokenImageExample {
     WebDriver driver;
@@ -17,7 +21,17 @@ public class BrokenImageExample {
     @Test
     public void findBrokenImageTest(){
         driver.get("https://the-internet.herokuapp.com/broken_images");
+        List<WebElement> images=driver.findElements(By.xpath("//div[@class='example']/img"));
 
+        int i=1;
+        for (WebElement image:images){
+            if (image.getAttribute("naturalWidth").equals("0")){
+                System.out.println("Image " + i +" is Broken");
+            }else {
+                System.out.println("Image " + i + " is not Broken");
+            }
+            i++;
+        }
     }
 
 }
