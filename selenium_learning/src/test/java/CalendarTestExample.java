6f8c5c9b1f4f3f42a04755f0e0ee1034cb9ed20f
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-02-25 21:37:06 +0530

    create and implement select future and previous date of the calendar

--- selenium_learning/src/test/java/CalendarTestExample.java
+++ selenium_learning/src/test/java/CalendarTestExample.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class CalendarTestExample {
     WebDriver driver;
 
@@ -20,8 +22,63 @@ public class CalendarTestExample {
     @Test
     public void CalendarTest(){
         driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='demo-frame']")));
+
+        //Method 01
+       /* WebElement datePicker=driver.findElement(By.id("datepicker"));
+        datePicker.sendKeys("02/27/2025");*/
+
+        //Method 02
         WebElement datePicker=driver.findElement(By.id("datepicker"));
-        datePicker.sendKeys("02/27/2025");
+        datePicker.click();
+
+        selectFutureDate("2026","October","6");
+        //selectPastDate("2024","May","8");
+
+    }
+
+
+    public void selectFutureDate(String year,String month,String day){
+        while (true){
+            String actualYear=driver.findElement(By.xpath("//span[@class='ui-datepicker-year']")).getText();
+            String actualMonth=driver.findElement(By.xpath("//span[@class='ui-datepicker-month']")).getText();
+
+            if (actualYear.equals(year)&& actualMonth.equals(month)){
+                break;
+            }else {
+                driver.findElement(By.xpath("//a[@title='Next']")).click();
+            }
+        }
+
+        List<WebElement> allDates=driver.findElements(By.xpath("//table[@class='ui-datepicker-calendar']/tbody/tr/td/a"));
+
+        for (WebElement dateElement:allDates){
+            if (dateElement.getText().equals(day)){
+                dateElement.click();
+                break;
+            }
+        }
+    }
+
+    public void selectPastDate(String year,String month,String day){
+        while (true){
+            String actualYear=driver.findElement(By.xpath("//span[@class='ui-datepicker-year']")).getText();
+            String actualMonth=driver.findElement(By.xpath("//span[@class='ui-datepicker-month']")).getText();
+
+            if (actualYear.equals(year)&& actualMonth.equals(month)){
+                break;
+            }else {
+                driver.findElement(By.xpath("//a[@title='Prev']")).click();
+            }
+        }
+
+        List<WebElement> allDates=driver.findElements(By.xpath("//table[@class='ui-datepicker-calendar']/tbody/tr/td/a"));
+
+        for (WebElement dateElement:allDates){
+            if (dateElement.getText().equals(day)){
+                dateElement.click();
+                break;
+            }
+        }
     }
 
 
