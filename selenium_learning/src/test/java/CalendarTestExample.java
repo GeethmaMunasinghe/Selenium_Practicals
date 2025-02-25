ff245d5b235a02b3c5e95f762b1f4b045e611bd4
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-02-25 20:36:15 +0530

    create CalendarTest method

--- selenium_learning/src/test/java/CalendarTestExample.java
+++ selenium_learning/src/test/java/CalendarTestExample.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -7,10 +9,19 @@ public class CalendarTestExample {
     WebDriver driver;
 
     @BeforeMethod
-    public void OpenCalendarPage(){
+    public void OpenCalendarPage() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","D:\\Level 4\\L4 S1\\Selenium Practice\\Driver\\chromedriver-win64\\chromedriver.exe");
         driver=new ChromeDriver();
         driver.manage().window().maximize();
+        driver.get("https://jqueryui.com/datepicker/");
+        Thread.sleep(3000);
+    }
+
+    @Test
+    public void CalendarTest(){
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='demo-frame']")));
+        WebElement datePicker=driver.findElement(By.id("datepicker"));
+        datePicker.sendKeys("02/27/2025");
     }
 
 
