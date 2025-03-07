cab3bc999442a36c28271202ad61148edf2c5a6b
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-03-07 12:03:45 +0530

    get all data from table

--- selenium_learning/src/test/java/web_table/WebTableExample.java
+++ selenium_learning/src/test/java/web_table/WebTableExample.java
@@ -19,12 +19,26 @@ public class WebTableExample {
 
     @Test
     public void WebTableTest(){
+        //how many rows in the table
         int rowCount=webDriver.findElements(By.xpath("//table[@id='productTable']/tbody/tr")).size();
         System.out.println("Rows Count: "+rowCount);
 
+        //how many columns in the table
         int columnCount=webDriver.findElements(By.xpath("//table[@id='productTable']/thead/tr/th")).size();
         System.out.println("Column Count: "+columnCount);
 
+        //retrieve the specific row/column data
+        String value=webDriver.findElement(By.xpath("//table[@id='productTable']/tbody/tr[3]/td[3]")).getText();
+        System.out.println("Specific row/column data: "+value);
+
+        //retrieve all the data from the table
+        for(int i=1;i<=rowCount;i++){           //rows
+            for (int j=1;j<columnCount;j++){   //columns
+                String tblData=webDriver.findElement(By.xpath("//table[@id='productTable']/tbody/tr["+i+"]/td["+j+"]")).getText();
+                System.out.println(tblData+" ");
+            }
+            System.out.println(); //for the line break
+        }
     }
 
 
