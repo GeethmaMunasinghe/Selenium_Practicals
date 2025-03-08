c66e16ba256afcccc688c979e39362a1ab6b9ac7
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-03-08 19:32:29 +0530

    create openJSExecutorPage method

--- selenium_learning/src/test/java/JavaScriptExecutorExample.java
+++ selenium_learning/src/test/java/JavaScriptExecutorExample.java
@@ -1,7 +1,15 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 
 public class JavaScriptExecutorExample {
     WebDriver driver;
 
+    @BeforeMethod
+    public void openJSExecutorPage(){
+        driver=new ChromeDriver();
+
+
+    }
 
 }
