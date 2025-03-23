130ff7025ad69d91ea885f3add9c0306c55b4284
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-03-23 07:58:38 +0530

    continue ImplicitWaitTest

--- selenium_learning/src/test/java/selenium_wait/ImplicitWaitDemo.java
+++ selenium_learning/src/test/java/selenium_wait/ImplicitWaitDemo.java
@@ -2,7 +2,10 @@ package selenium_wait;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -17,16 +20,20 @@ public class ImplicitWaitDemo {
     public void openTablePage(){
         System.setProperty("webdriver.chrome.driver","D:\\Level 4\\L4 S1\\Selenium Practice\\Driver\\chromedriver-win64\\chromedriver.exe");
         driver=new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //till the driver.quit this will affect.here exception will hold
+       // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //till the driver.quit this will affect.here exception will hold
         driver.manage().window().maximize();
         driver.get("https://www.leafground.com/waits.xhtml");
     }
 
     @Test
-    public void ImplicitWaitTest() throws InterruptedException {
+    public void ImplicitWaitTest()  {
+        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(10));
         driver.findElement(By.xpath(" //button[@id='j_idt87:j_idt89']")).click();
+        By newBtnLocator=By.xpath("//button[@id='j_idt87:j_idt90']/span");
 
-        String newBtnText=driver.findElement(By.xpath("//button[@id='j_idt87:j_idt90']/span")).getText();
+
+        WebElement newBtnElement=wait.until(ExpectedConditions.visibilityOfElementLocated(newBtnLocator));
+        String newBtnText=newBtnElement.getText();
         System.out.println("New button text is: "+newBtnText);
 
     }
