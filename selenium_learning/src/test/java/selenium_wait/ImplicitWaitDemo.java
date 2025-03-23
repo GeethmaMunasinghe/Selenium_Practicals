2eae8aac452e8e083d3ee3e4a6813c9bbec60528
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-03-23 07:07:37 +0530

    create afterMethod

--- selenium_learning/src/test/java/selenium_wait/ImplicitWaitDemo.java
+++ selenium_learning/src/test/java/selenium_wait/ImplicitWaitDemo.java
@@ -3,6 +3,7 @@ package selenium_wait;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -16,17 +17,23 @@ public class ImplicitWaitDemo {
     public void openTablePage(){
         System.setProperty("webdriver.chrome.driver","D:\\Level 4\\L4 S1\\Selenium Practice\\Driver\\chromedriver-win64\\chromedriver.exe");
         driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //till the driver.quit this will affect.here exception will hold
         driver.manage().window().maximize();
-        driver.manage().timeouts().getPageLoadTimeout();
         driver.get("https://www.leafground.com/waits.xhtml");
     }
 
     @Test
-    public void ImplicitWaitTest(){
+    public void ImplicitWaitTest() throws InterruptedException {
         driver.findElement(By.xpath(" //button[@id='j_idt87:j_idt89']")).click();
+
         String newBtnText=driver.findElement(By.xpath("//button[@id='j_idt87:j_idt90']/span")).getText();
         System.out.println("New button text is: "+newBtnText);
 
+    }
+
+    @AfterMethod
+    public void closeBrowser(){
+        driver.quit();
     }
 
 }
