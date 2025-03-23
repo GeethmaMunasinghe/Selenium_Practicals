18b5f0c2f2fd7483c404e9fd35eab7ea50eab6dc
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-03-23 08:28:14 +0530

    contd. fluentwait

--- selenium_learning/src/test/java/selenium_wait/FluentWaitDemo.java
+++ selenium_learning/src/test/java/selenium_wait/FluentWaitDemo.java
@@ -1,10 +1,13 @@
 package selenium_wait;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -26,13 +29,15 @@ public class FluentWaitDemo {
 
     @Test
     public void fluentWaitTest(){
-        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        //Declaration(Define fluent wait)
+        Wait<WebDriver> wait=new FluentWait<WebDriver>(driver)
+                                .withTimeout(Duration.ofSeconds(30))       //maximum time to wait
+                                .pollingEvery(Duration.ofSeconds(5))       //frequency to check the condition
+                                .ignoring(NoSuchElementException.class);   //ignore specific condition
+
+
         driver.findElement(By.xpath(" //button[@id='j_idt87:j_idt89']")).click();
-        By newBtnLocator=By.xpath("//button[@id='j_idt87:j_idt90']/span");
-
-
-        WebElement newBtnElement=wait.until(ExpectedConditions.visibilityOfElementLocated(newBtnLocator));
-        String newBtnText=newBtnElement.getText();
+        String newBtnText=driver.findElement(By.xpath("//button[@id='j_idt87:j_idt90']/span")).getText();
         System.out.println("New button text is: "+newBtnText);
     }
 
